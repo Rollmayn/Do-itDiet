456944690c77cea43d9c3cb53f2a16e34e9bd5e0
Rollmayn <stefanroman_@hotmail.com>
2020-11-07 15:27:12 +0000

    Initial commit

---
+++ app/src/main/java/com/example/sedd/Splash.java
@@ -0,0 +1,39 @@
+package com.example.sedd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Splash extends AppCompatActivity {
+
+    //Variables
+    private Button btnBegin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //Initializes application in fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //makes button interactable, initializes openLogin() function when clicked.
+        btnBegin = (Button) findViewById(R.id.btnBegin);
+        btnBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //openLogin function recalled in onClick function
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+    }
+
+    //function to open Login activity once button clicked
+    public void openLogin(){
+        Intent startApp = new Intent(Splash.this, Login.class);
+        startActivity(startApp);
+    }
+}
