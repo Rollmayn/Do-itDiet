8528bfe6bacb533eacdb1671b9735b9dad6d5dae
Rollmayn <stefanroman_@hotmail.com>
2020-11-07 18:54:49 +0000

    homepage and menu fragments/layouts complete

---
+++ app/src/main/java/com/example/sedd/homepage.java
@@ -0,0 +1,26 @@
+package com.example.sedd;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
--- app/src/main/java/com/example/sedd/signin.java
+++ app/src/main/java/com/example/sedd/signin.java
@@ -1,16 +1,28 @@
 package com.example.sedd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signin extends AppCompatActivity {
 
-    private Button btnLogin, btnCreateAccount;
+    //Variables
+    EditText emailEnter, passwordEnter;
+    Button btnLogin, btnCreateAccount;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +31,44 @@ public class signin extends AppCompatActivity {
         //Initializes application in fullscreen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        emailEnter = findViewById(R.id.emailEnter);
+        passwordEnter = findViewById(R.id.passwordEnter);
+        mAuth = FirebaseAuth.getInstance();
+
         btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEnter.getText().toString().trim();
+                String password = passwordEnter.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    emailEnter.setError("Email is required!");
+                    return;
+                } else if (TextUtils.isEmpty(password)) {
+                    passwordEnter.setError("Password is required!");
+                    return;
+                } else if (password.length() > 6) {
+                    passwordEnter.setError("Password must be more than 6 characters!");
+                    return;
+                }
+
+                //Authenticate users
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(signin.this, "Logged in!!", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), home.class));
+                        } else {
+                            Toast.makeText(signin.this, "Error occurred. Please try again." + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        //Initialize account creation activity
         btnCreateAccount = (Button) findViewById(R.id.btnCreateAccount);
         btnCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
